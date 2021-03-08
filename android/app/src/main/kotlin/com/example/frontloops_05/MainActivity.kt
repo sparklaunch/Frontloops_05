39c6b982ee814ca0b85cadea7190831dbf20419c
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-03-08 17:21:48 +0900

    initialize

---
+++ android/app/src/main/kotlin/com/example/frontloops_05/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.frontloops_05
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Frontloops());
+}
+
+class Frontloops extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
